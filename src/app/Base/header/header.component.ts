2913485755995f1f45ae6a6dde0d96afdb026b23
Gopika <gopikakg@techversantinfotech.com>
2024-10-22 14:39:49 +0530

    header ui

--- src/app/Base/header/header.component.ts
+++ src/app/Base/header/header.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  companyName = '31 INCORPORATED';
+  companyId = 4610743;
+  isMenuOpen :boolean = false;
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+
+  get companyNameInitials(): string {
+    return this.companyName
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  switchCompany() {
+    console.log('Switch company clicked');
+  }
+
+  logout() {
+    console.log('Logout clicked');
+  }
 
 }
