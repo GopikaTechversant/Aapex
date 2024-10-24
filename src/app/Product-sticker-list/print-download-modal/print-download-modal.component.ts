ad53965a5df42f2377eb1a70b83801a9f3778232
Gopika <gopikakg@techversantinfotech.com>
2024-10-24 15:24:58 +0530

    formats for print download

--- src/app/Product-sticker-list/print-download-modal/print-download-modal.component.ts
+++ src/app/Product-sticker-list/print-download-modal/print-download-modal.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef ,MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-print-download-modal',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,NgIf],
   templateUrl: './print-download-modal.component.html',
   styleUrl: './print-download-modal.component.css'
 })
 export class PrintDownloadModalComponent implements OnInit{
-  selectedFormat: string = 'svg';
-  constructor(private dialogRef: MatDialogRef<PrintDownloadModalComponent>){}
+  selectedFormat: string = '';
+  selectedType:string = '';
+  constructor(private dialogRef: MatDialogRef<PrintDownloadModalComponent>,@Inject(MAT_DIALOG_DATA) public data: any){}
   ngOnInit(): void {
+    this.selectedType = this.data;
+    if(this.selectedType === 'print') this.selectedFormat = '2X2';
+    else if(this.selectedType === 'download') this.selectedFormat = 'svg';
   }
 
   onSubmit():void{
