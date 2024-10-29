c76cb5ec52dcbe9901bcf5732d8dc82de9868628
Gopika <gopikakg@techversantinfotech.com>
2024-10-29 17:23:26 +0530

    added router in left sidebar

--- src/app/Base/left-sidebar/left-sidebar.component.ts
+++ src/app/Base/left-sidebar/left-sidebar.component.ts
@@ -3,6 +3,7 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { NgClass } from '@angular/common';
 import { PrintDownloadModalComponent } from '../../Product-sticker-list/print-download-modal/print-download-modal.component';
+import { Router } from '@angular/router'; // Import Router
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
@@ -22,9 +23,12 @@ import { PrintDownloadModalComponent } from '../../Product-sticker-list/print-do
 export class LeftSidebarComponent {
 
   selectedItem: string = 'dashboard';
-  constructor(private dialogRef: MatDialogRef<LeftSidebarComponent>,private dialog: MatDialog) {}
+  constructor(private dialogRef: MatDialogRef<LeftSidebarComponent>,private dialog: MatDialog,private router: Router) {}
   selectItem(item: string) {
     this.selectedItem = item;
+    if (this.selectedItem === 'contactUs') {
+      this.router.navigate(['/contactUs']);
+    }
 }
 printDownloadProductModal(selectedType:any):void{
   const dialogRef = this.dialog.open(PrintDownloadModalComponent, {
