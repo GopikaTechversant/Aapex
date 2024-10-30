962ec82237efcb726b02debd1ce00793cb5bb179
Gopika <gopikakg@techversantinfotech.com>
2024-10-30 18:51:00 +0530

    product table api integration

--- src/app/Product-sticker-list/product-list-table/product-list-table.component.ts
+++ src/app/Product-sticker-list/product-list-table/product-list-table.component.ts
@@ -4,51 +4,31 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { PrintDownloadModalComponent } from '../print-download-modal/print-download-modal.component';
 import { AddQrContentModalComponent } from '../add-qr-content-modal/add-qr-content-modal.component';
 import { ApiServiceService } from '../../services/api-service.service';
+import { DatePipe } from '@angular/common';
+import { log } from 'console';
 @Component({
   selector: 'app-product-list-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,DatePipe],
+  providers: [DatePipe],
   templateUrl: './product-list-table.component.html',
   styleUrl: './product-list-table.component.css'
 })
 export class ProductListTableComponent implements OnInit {
-
+  stickerCount: number = 0;
   products_list: any[] = [
-    {
-      name: 'test product copy',
-      code: 'XQNF-08-055-23',
-      product_image: 'assets/images/product1.png',
-      // qrCode: 'assets/images/qr1.png',
-      lastUpdated: 'Oct 14, 2024',
-      total_images: 2,
-      total_videos: 3,
-      total_files: 0
-    },
-    {
-      name: '',
-      code: '',
-      product_image: '',
-      // qrCode: 'assets/images/qr1.png',
-      lastUpdated: 'Oct 14, 2024',
-    },
-    {
-      name: 'test product copy',
-      code: 'XQNF-08-055-23',
-      product_image: 'assets/images/product1.png',
-      // qrCode: 'assets/images/qr1.png',
-      lastUpdated: 'Oct 14, 2024',
-      total_images: 0,
-      total_videos: 0,
-      total_files: 0
-    }
+    
 
   ]
-  constructor(private dialog: MatDialog, private apiService: ApiServiceService) { }
+  constructor(private dialog: MatDialog, private apiService: ApiServiceService,private datePipe: DatePipe) { }
   ngOnInit(): void {
+    this.fetchStickerCount();
     this.apiService.get(`/v1/exhibitor/products?offset=1&pageCount=15&sortkey=1&sortvalue=ps.sCreatedDateTime&iStickerSetId=0&iAssignedFilter=2`).subscribe((res: any) => {
-      console.log("tresponse", res);
-
+     
+      this.products_list = res?.productStickerBos;
+      console.log("tresponse", this.products_list);
     })
+  
   }
 
   printDownloadProductModal(selectedType: any): void {
@@ -70,4 +50,17 @@ export class ProductListTableComponent implements OnInit {
     })
   }
 
+  fetchStickerCount():void{
+    this.apiService.get(`/v1/exhibitor/sticker/count?name=&setId=0`).subscribe((res:any) => {
+      console.log("sticker",res.lPurchasedStickerCount);
+      this.stickerCount = res.lPurchasedStickerCount || 0;
+
+      // Filter or slice the products list based on the sticker count
+      this.products_list = this.products_list.slice(0, this.stickerCount);
+    })
+  }
+
+  formatDate(date: string | null): string {
+    return this.datePipe.transform(date, 'MMM d, y') || ''; // Format to 'Oct 11, 2024'
+  }
 }
