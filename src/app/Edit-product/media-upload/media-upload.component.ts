da3e452459e7448b3fb45407549355d660c21f45
RemyaRajA1998 <remya@techversantinfo.com>
2024-11-12 12:19:26 +0530

    updated file uplaod

--- src/app/Edit-product/media-upload/media-upload.component.ts
+++ src/app/Edit-product/media-upload/media-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-media-upload',
@@ -8,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './media-upload.component.css'
 })
 export class MediaUploadComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
 
+  onUploadClick() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFilesSelected(event: any) {
+    const files = event.target.files || event.dataTransfer.files;
+    if (files.length > 0) {
+      this.handleFiles(files);
+    }
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer?.files.length) {
+      this.handleFiles(event.dataTransfer.files);
+    }
+  }
+
+  private handleFiles(files: FileList) {
+    Array.from(files).forEach(file => {
+      console.log('File:', file.name);
+    });
+  }
 }
